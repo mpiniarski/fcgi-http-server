c707df37b61db274e04347c0462475b272c122fc
Joanna Sołomiewicz <joanna.solomiewicz@student.put.poznan.pl>
2017-01-20 19:15:04 +0100

    FCGI uri change

--- fcgi-app/app/app.py
+++ fcgi-app/app/app.py
@@ -4,22 +4,25 @@ import string
 import random
 import time
 import os.path
+
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/hello-world')
 def hello_world():
     time.sleep(5)
-    return 'Hello Asia!'
+    return 'Hello world. This is awesome FCGI!'
 
-@app.route('/the-time')
+
+@app.route('/api/the-time')
 def the_time():
     cur_time = str(datetime.now())
     return cur_time + ' is the current time!  ...YEAH!'
 
-@app.route('/long/<int:size>')
+
+@app.route('/api/chars/<int:size>')
 def image(size):
     return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(size))
-
 
 
 if __name__ == '__main__':
